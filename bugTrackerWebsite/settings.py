71e4eb98ad4ed43f7587012d3192a1cccef199ed
jaymaverick <117550425+jaymaverick@users.noreply.github.com>
2023-02-23 02:38:46 +0200

    Update settings.py

--- bugTrackerWebsite/settings.py
+++ bugTrackerWebsite/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['web-production-999c.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-999c.up.railway.app']
+ALLOWED_HOSTS = ['web-production-6abe.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-6abe.up.railway.app']
 
 # Application definition
 
